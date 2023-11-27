6fc430733df65793cd0edddb55c9232ae66745c3
TochnoChopper <146355450+TochnoChopper@users.noreply.github.com>
2023-11-27 13:47:30 +0300

    Update Questarium.cpp

--- Questarium.cpp
+++ Questarium.cpp
@@ -1,85 +1,37 @@
-﻿#include <string>
+#include <string>
 #include <iostream>
 #include "Player.h"
-#include "Projectiles.h"
 #include "Enemy.h"
-#include "Magic.h"
 #include "Weapons.h"
+#include "Quest.h"
+#include "Entity.h"
 
 using namespace std;
-
-class entity {
-protected:
-    string name_e, type_e;
-    bool is_alive;
-public:
-    entity(string name, string type, bool is_alive) {
-        name_e = name;
-        type_e = type;
-        this->is_alive = is_alive;
-    }
-    friend void Cut_tree(player&, entity&);
-    friend void Earning_coin(player&, entity&);
-
-};
-
-class quest {
-protected:
-    unsigned short number;
-    string name, description;
-public:
-    quest(unsigned short number, string name, string description) {
-        this->number = number;
-        this->name = name;
-        this->description = description;
-    }
-
-    void Quest_get_info() {
-        cout << "Name = " << name << "; Number = " << number << endl << "Description: " << description << endl;
-    }
-};
-
-void Cut_tree(player& d, entity& x) {
-    if (x.type_e == "Tree") {
-        cout << d.player_name << " is cutting " << x.name_e << endl;
-    }
-}
-
-void Earning_coin(player& d, entity& x) {
-    if (x.type_e == "Coin") {
-        cout << d.player_name << " earned " << x.name_e << endl;
-    }
-}
-
 
 int main()
 {
     // Все далее лишь демонстрирует возможности взаимодействия классов
 
     quest FirstQuest = quest(1, "Begining", "You need to kill 2 red demons");
-    FirstQuest.Quest_get_info();
+    NPC Valera = NPC("Valera", 32, "Stranger, pls, help me!", FirstQuest);
 
-    warrior Sasha = warrior(212, "Sasha", 120);
-    red_demon Grisha;
-    Sasha.Warrior_get_info();
+    warrior Misha = warrior(213223, "Misha", 100);
 
-    sniper Hopper = sniper(192102, "Hopper", 110);
-    Hopper.Sniper_get_info();
+    weapon legend_bow = weapon("bow", "Legengary bow", 30, 20);
+    weapon My_sword = weapon("sword", "Legengary sword", 45, 10);
 
-    Sasha.Warior_attack_RD(Grisha);
-    cout << Grisha.health_e << endl;
+    Misha.Talk_NPC(Valera);
 
-    blood_mage Nomad = blood_mage(432, "Pavel", 120);
-    Nomad.lvl_up(2); // Уровень не повышается (Fixed)
-    Nomad.Blood_mage_attack_RD(Grisha);
-    cout << Grisha.health_e << endl;
-    Nomad.Blood_mage_get_info();
+    Misha.Swap_weapon(My_sword);
+    Misha.Warrior_furios();
+    Misha.Player_get_info();
 
-    entity Tree = entity("Grand Tree", "Tree", true);
-    entity Money = entity("coin", "Coin", false);
-    Cut_tree(Nomad, Tree);
-    Earning_coin(Nomad, Money);
+    demon Vanya;
+    Vanya.Enemy_get_info();
 
+    Misha.Player_attack(Vanya);
+
+    Vanya.Enemy_get_info();
     
-    return 1;
+    return 0;
 }
